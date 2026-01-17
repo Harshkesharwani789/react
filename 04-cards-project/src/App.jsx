09c9085a959980608583f72b134084553c781497
Harshkesharwani789 <atul123564@gmail.com>
2026-01-17 15:43:59 +0530

    till cards project

---
+++ 04-cards-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./components/Card";
+import User from "./components/User";
+
+const App = () => {
+  const arr = [10, 20, 30, 40];
+  return (
+    <div className="parent">
+      {arr.map(function (elem) {
+        return elem * 2;
+      })}
+    </div>
+  );
+};
+
+export default App;
